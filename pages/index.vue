73bcdd575896bfea378c9ca9f64a1282328bdb82
Ashraful Islam <25670420+ashraful19@users.noreply.github.com>
2026-01-07 00:47:53 +0600

    Poll posts from the API each 30 seconds

--- pages/index.vue
+++ pages/index.vue
@@ -7,15 +7,35 @@ const user = useUser()
 const postStore = usePostStore()
 const favoriteStore = useFavoriteStore()
 
+let pollPostsInterval = null
+
 onMounted(async () => {
   await postStore.getPosts()
   favoriteStore.getFavorites()
+
+  pollPostsInterval = setInterval(() => {
+    postStore.getPollPosts()
+  }, 30000)
+})
+
+onUnmounted(() => {
+  if (pollPostsInterval) {
+    clearInterval(pollPostsInterval)
+    pollPostsInterval = null
+  }
 })
 </script>
 
 <template>
   <PostForm
     v-if="!user.isGuest" />
+  <div class="flex justify-end">
+    <button v-if="postStore.hasNewPosts"
+      class="bg-gray-600 text-white px-8 py-2 rounded-lg"
+      @click="postStore.loadNewPosts()">
+      Load New Posts
+    </button>
+  </div>
   <div class="grid gap-16">
     <PostItem
       v-for="post in postStore.posts"
--- stores/post.js
+++ stores/post.js
@@ -2,10 +2,11 @@ export const usePostStore = defineStore('post', () => {
     const { $api } = useNuxtApp()
 
     const posts = ref([])
+    const pollPosts = ref([])
 
     async function getPosts() {
         const { data } = await $api.get('posts')
-        posts.value = data
+        posts.value = data || []
     }
 
     async function createPost(post) {
@@ -13,9 +14,51 @@ export const usePostStore = defineStore('post', () => {
         posts.value.unshift(data)
     }
 
+    async function getPollPosts() {
+        try {
+            const { data } = await $api.get('posts')
+            pollPosts.value = data || []
+        } catch (error) {
+            pollPosts.value = []
+        }
+    }
+
+    function loadNewPosts() {
+        if (!hasNewPosts.value) {
+            return
+        }
+
+        const currentPostIds = posts.value.map(post => post.id)
+        const newPosts = pollPosts.value.filter(post => !currentPostIds.includes(post.id))
+        
+        if (newPosts.length > 0) {
+            posts.value = [...newPosts, ...posts.value]
+        }
+        
+        pollPosts.value = []
+    }
+
+    const hasNewPosts = computed(() => {
+        if (!pollPosts.value.length) {
+            return false
+        }
+
+        if (pollPosts.value.length > posts.value.length) {
+            return true
+        }
+
+        const currentPostIds = posts.value.map(post => post.id)
+        return pollPosts.value.some(post => !currentPostIds.includes(post.id))
+    })
+
+
     return {
         posts,
+        pollPosts,
         getPosts,
-        createPost
+        createPost,
+        getPollPosts,
+        loadNewPosts,
+        hasNewPosts
     }
 })
